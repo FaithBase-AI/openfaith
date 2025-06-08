cae2f94e241df4fc8ff6ec09ac8903b42b241a84
Izak Filmalter <izak@steeple.works>
2025-06-08 06:28:20 -0400

    Remove old code.

--- backend/api/router/adapterRouter.ts
+++ backend/api/router/adapterRouter.ts
@@ -1,11 +1,10 @@
 import { FetchHttpClient } from '@effect/platform'
 import * as PgDrizzle from '@effect/sql-drizzle/Pg'
+import { adaptersApi } from '@openfaith/api/adapters/adaptersApi'
 import { createTRPCRouter, orgProcedure } from '@openfaith/api/trpc'
 import { adapterDetailsTable, adapterTokenTable, DBLive } from '@openfaith/db'
-import { fetchPcoTokenE } from '@openfaith/pco/server'
-import { asyncNoOp, env } from '@openfaith/shared'
+import { asyncNoOp } from '@openfaith/shared'
 import { Effect, Option, pipe, Record, Schema } from 'effect'
-import { adaptersApi } from './../adapters/adaptersApi'
 
 export const AdapterConnectRequest = Schema.Struct({
   adapter: Schema.String,
@@ -75,53 +74,6 @@ export const adapterRouter = createTRPCRouter({
               ),
           }),
         )
-
-        switch (input.adapter) {
-          case 'pco': {
-            const token = await Effect.gen(function* () {
-              const db = yield* PgDrizzle.PgDrizzle
-
-              const token = yield* fetchPcoTokenE({
-                clientId: env.VITE_PLANNING_CENTER_CLIENT_ID,
-                clientSecret: env.PLANNING_CENTER_SECRET,
-                code: input.code,
-                grantType: 'authorization_code',
-                redirectUri: input.redirectUri,
-              })
-
-              const createdAt = new Date()
-
-              yield* Effect.all([
-                db.insert(adapterTokenTable).values({
-                  accessToken: token.accessToken,
-                  adapter: 'pco',
-                  createdAt,
-                  expiresIn: token.expiresIn,
-                  orgId,
-                  refreshToken: token.refreshToken,
-                  userId,
-                }),
-                db.insert(adapterDetailsTable).values({
-                  adapter: 'pco',
-                  createdAt,
-                  enabled: true,
-                  orgId,
-                  syncStatus: [],
-                }),
-              ])
-            }).pipe(
-              Effect.provide(FetchHttpClient.layer),
-              Effect.provide(DBLive),
-              Effect.catchAllCause(Effect.logError),
-              Effect.runPromise,
-            )
-
-            console.log(token)
-            break
-          }
-          default:
-            break
-        }
 
         console.log('adapterConnect finish')
       } catch (error) {
