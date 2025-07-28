c9432fb8b47b135547712f3fb74a7defd0c41592
Izak Filmalter <izak@steeple.works>
2025-07-28 15:54:22 -0700

    Delete globalState.tsx

--- packages/ui/shared/globalState.tsx
+++
@@ -1,6 +0,0 @@
-import { atom } from 'jotai'
-
-export const isSmScreenAtom = atom<boolean>(true)
-export const isMdScreenAtom = atom<boolean>(true)
-export const isLgScreenAtom = atom<boolean>(true)
-export const isXlScreenAtom = atom<boolean>(true)
