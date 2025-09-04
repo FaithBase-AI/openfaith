25973e8cb4878f1b7f77fa884cbe1aa9c4793b11
Ben Linzel <ben@americancloud.io>
2025-09-04 14:12:19 -0400

    Update authClient.ts

--- packages/auth/authClient.ts
+++ packages/auth/authClient.ts
@@ -1,12 +1,21 @@
-import { emailChangeOTPClient } from '@openfaith/auth/plugins'
-import { adminClient, emailOTPClient, organizationClient } from 'better-auth/client/plugins'
-import { createAuthClient } from 'better-auth/react'
+import { emailChangeOTPClient } from "@openfaith/auth/plugins";
+import {
+  adminClient,
+  emailOTPClient,
+  organizationClient,
+} from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_ZERO_SERVER,
-  plugins: [emailOTPClient(), adminClient(), organizationClient(), emailChangeOTPClient()],
-})
+  plugins: [
+    emailOTPClient(),
+    adminClient(),
+    organizationClient(),
+    emailChangeOTPClient(),
+  ],
+});
 
-export type Session = typeof authClient.$Infer.Session
-export type ActiveOrg = typeof authClient.$Infer.ActiveOrganization
-export type SlimOrg = typeof authClient.$Infer.Organization
+export type Session = typeof authClient.$Infer.Session;
+export type ActiveOrg = typeof authClient.$Infer.ActiveOrganization;
+export type SlimOrg = typeof authClient.$Infer.Organization;
