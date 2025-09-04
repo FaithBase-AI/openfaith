1aa5cc6546730ad89e06f130438c87b113895d37
Ben Linzel <ben@americancloud.io>
2025-09-03 20:27:59 -0400

    fix containers

--- apps/openfaith/vite.config.ts
+++ apps/openfaith/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      external: ["@effect/experimental/Reactivity"],
+      external: ["@effect/experimental"],
     },
   },
   server: {
